a424c657f912d90f0ddbbe0cbc3f3614f5dd5e23
wngjs_L2e <gjsdlaka0012@gmail.com>
2023-04-11 06:15:13 +0000

    sdfsdfsdf

---
+++ 47.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+	
+	int num1 = 14; //변수선언 및 초기화
+	int num2 = 3;
+	int sum2 = 0; //초기화
+	
+	sum = num1 + num2;
+	printf("%d + %d = %d \n", num1,num2, sum);
+	printf("%d - %d = %d \n", num1,num2, sum);
+	printf("%d * %d = %d \n", num1,num2, sum);
+	return 0;
+}
